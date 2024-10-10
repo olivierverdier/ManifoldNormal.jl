62355956a4a079ee6ea621ad06959d1624309354
Olivier Verdier <olivier.verdier@gmail.com>
2024-10-10 12:40:03 +0200

    ENH: NoProcessNoise missing methods

--- src/Noise/NoActionNoise.jl
+++ src/Noise/NoActionNoise.jl
@@ -7,8 +7,14 @@ end
 
 const NoProcessNoise{TA} = NoActionNoise{TA}
 
-get_lie_covariance_at(n::NoActionNoise, ::Any, ::Any) = let d = manifold_dimension(base_group(n.action))
+get_lie_covariance_at(n::NoActionNoise, ::Any) = let d = manifold_dimension(base_group(n.action))
     return SparseArrays.spzeros(d, d)
 end
 
-# TODO: this implementation is incomplete
+get_lie_covariance_at(n::NoActionNoise, x::Any, ::Any) = get_lie_covariance_at(n, x)
+
+add_noise(::NoActionNoise, ::Random.AbstractRNG, x) = x
+
+rescale_noise(n::NoActionNoise, ::Any) = n
+
+# TODO: `get_covariance_at` is missing
